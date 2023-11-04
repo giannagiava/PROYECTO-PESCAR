2fb753baf85d6c151e53fdaffff0495c2252f209
“Gianna <giannagiavarini@outlook.com>
2023-11-04 18:19:56 -0300

    agregamos carrousel con imagenes

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,5 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var sliderContainer = document.querySelector('.slider-container');
+  var items = sliderContainer.querySelectorAll('li');
+  var interval = 2000; // Intervalo en milisegundos (2 segundos en este ejemplo)
+  var currentIndex = 0;
 
-  $(document).ready(function(){
-    $('.carousel').carousel();
-  });
+  function showNextSlide() {
+    items[currentIndex].style.display = 'none'; // Oculta la imagen actual
+    currentIndex = (currentIndex + 1) % items.length;
+    items[currentIndex].style.display = 'block'; // Muestra la siguiente imagen
+  }
 
+  setInterval(showNextSlide, interval);
+});
