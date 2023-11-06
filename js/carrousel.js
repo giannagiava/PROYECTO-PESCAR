ceda897219aff6db6f164aef9085cbc6c6b0a094
“Gianna <giannagiavarini@outlook.com>
2023-11-06 18:58:40 -0300

    agregamos cambios a css

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let sliderContainer = document.querySelector('.slider-container');
-  let items = sliderContainer.querySelectorAll('li');
-  let interval = 2000; // Intervalo en milisegundos (2 segundos en este ejemplo)
-  let currentIndex = 0;
+// document.addEventListener('DOMContentLoaded', function () {
+//   let sliderContainer = document.querySelector('.slider-container');
+//   let items = sliderContainer.querySelectorAll('li');
+//   let interval = 2000; // Intervalo en milisegundos (2 segundos en este ejemplo)
+//   let currentIndex = 0;
 
-  function showNextSlide() {
-    items[currentIndex].style.display = 'none'; // Oculta la imagen actual
-    currentIndex = (currentIndex + 1) % items.length;
-    items[currentIndex].style.display = 'block'; // Muestra la siguiente imagen
-  }
+//   function showNextSlide() {
+//     items[currentIndex].style.display = 'none'; // Oculta la imagen actual
+//     currentIndex = (currentIndex + 1) % items.length;
+//     items[currentIndex].style.display = 'block'; // Muestra la siguiente imagen
+//   }
 
-  setInterval(showNextSlide, interval);
-});
+//   setInterval(showNextSlide, interval);
+// });
